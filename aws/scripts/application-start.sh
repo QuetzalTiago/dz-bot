59121c7ce3f16935e23f6a2b5b467eae51c2658e
Tiago <quetzaltiago@gmail.com>
2023-07-04 22:04:40 -0300

    Fixed app-start.sh

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -2,5 +2,5 @@ source ~/.bashrc
 cd /dz-bot/bot 
 npm install typescript
 npm install
-npm run commands:register &
-npm start
+npm run commands:register > /dev/null 2> /dev/null < /dev/null &
+npm start > /dev/null 2> /dev/null < /dev/null &

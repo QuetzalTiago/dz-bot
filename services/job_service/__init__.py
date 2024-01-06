7369732c53c26214c240d74501622218f3ac3b4b
Tiago <quetzaltiago@gmail.com>
2024-01-06 19:54:09 -0300

    fixed jobs

--- services/job_service/__init__.py
+++ services/job_service/__init__.py
@@ -15,16 +15,16 @@ class JobService:
         print("Job service initialized.")
         self.is_running = True
         while self.is_running:
+            current_time = time.time()
             for job in self.jobs:
-                current_time = time.time()
-                # Check if the job should run immediately at initialization
-                if job.init_run and job.last_run is None:
+                # If the job never ran, set the last_run to the current time
+                if job.last_run is None:
+                    job.last_run = current_time
+
+                # Check if the job should run based on the interval
+                if current_time - job.last_run >= job.interval:
                     asyncio.create_task(job.run())
-                elif (
-                    job.last_run is not None
-                    and current_time - job.last_run >= job.interval
-                ):
-                    asyncio.create_task(job.run())
+
             await asyncio.sleep(1)  # Sleep a bit before checking the jobs again
 
     def stop(self):

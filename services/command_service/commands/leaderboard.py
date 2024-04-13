213209f8b9759258adbf5c4faa531610af24f9d4
Tiago <quetzaltiago@gmail.com>
2024-04-13 12:39:44 -0300

    fixed leaderboard

--- services/command_service/commands/leaderboard.py
+++ services/command_service/commands/leaderboard.py
@@ -15,24 +15,24 @@ class LeaderboardCommand(BaseCommand):
     async def execute(self):
         user_hours_list = self.db_service.get_all_user_hours()
 
+        # Filter out the bot's own user ID to prevent it from appearing in the leaderboard
+        bot_user_id = self.client.user.id
+        user_hours_list = [
+            user_hour for user_hour in user_hours_list if user_hour[0] != bot_user_id
+        ]
+
         sorted_user_hours = sorted(user_hours_list, key=lambda x: x[1], reverse=True)[
             :5
         ]  # Get top 5
 
         leaderboard_message = "🏆 **Leaderboard** 🏆\n\n"
-
         for index, (user_id, hours) in enumerate(sorted_user_hours, start=1):
             member = await self.client.fetch_user(user_id)
-            if member:
-                username = member.name
-            else:
-                username = f"ID: {user_id}"
-
+            username = member.name if member else f"ID: {user_id}"
             leaderboard_message += (
                 f"**#{index} {username}** - {round(hours, 2)} hours\n"
             )
 
         await self.message.channel.send(leaderboard_message)
-
         await self.message.clear_reactions()
         await self.message.add_reaction("✅")

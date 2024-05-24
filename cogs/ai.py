4370cd462e5ea3e6095523672b65c0ab84d9825e
Tiago <quetzaltiago@gmail.com>
2024-05-24 20:57:55 -0300

    fixed initial prompt

--- cogs/ai.py
+++ cogs/ai.py
@@ -11,18 +11,21 @@ class AI(commands.Cog):
         self.bot = bot
         self.config = config
         self.ai = genai.configure(api_key=config["secrets"]["google_api_ai"])
-        self.model = genai.GenerativeModel('gemini-pro')
+        self.model = genai.GenerativeModel("gemini-pro")
+        self.initial_prompt = "You are DJ Khaled a Discord bot impersonating DJ Khaled, without a command prefix, designed for music, chess (this is not to play against you, but to facilitate a link for two players), and utility functionalities. It recognizes and processes commands like play, skip, loop, stop, clear, queue, purge, restart (this is for the entire bot to restart not music), help, btc, emoji, and chess directly. The bot operates in a Discord server environment, focusing on clarity and ease of use. This a conversation between you and a user. In every response, include aspects of DJ Khaleds personality, Respond in the same language of the user. Only give one response, dont make text for the user, when responding, dont use '[DJ Khaled]:'. [user]: "
 
     @staticmethod
     def to_markdown(text):
-        text = text.replace('•', '  *')
-        return textwrap.indent(text, '> ', predicate=lambda _: True)
+        text = text.replace("•", "  *")
+        return textwrap.indent(text, "> ", predicate=lambda _: True)
 
     @commands.hybrid_command()
     async def ask(self, ctx, question):
         """Returns an answer to a question"""
         await ctx.message.add_reaction("⌛")
-        response = self.model.generate_content(question)
+        user_prompt = ctx.message.content[4:]
+        print(user_prompt)
+        response = self.model.generate_content(self.initial_prompt + user_prompt)
         await ctx.send(self.to_markdown(response.text))
         await ctx.message.clear_reactions()
         await ctx.message.add_reaction("✅")

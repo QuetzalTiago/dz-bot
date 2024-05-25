a73e90ca72aba1815cb738443309bca00bff2542
Tiago <quetzaltiago@gmail.com>
2024-05-24 22:20:10 -0300

    fixed gemini ubuntu

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pip3 install -r /dz-bot/requirements.txt
+python3.9 -m pip install -r /dz-bot/requirements.txt
 cd /dz-bot/
-python3 bot.py > output.txt 2> error.txt < /dev/null &
+python3.9 bot.py > output.txt 2> error.txt < /dev/null &

a16ef14b9f5bd945885aec877ab672809d8da3c6
Tiago <quetzaltiago@gmail.com>
2024-07-04 19:29:34 -0300

    fixed app start script

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python3.9 -m pip install -r /dz-bot/requirements.txt
+python3 -m pip install -r /dz-bot/requirements.txt
 cd /dz-bot/
-python3.9 bot.py > output.txt 2> error.txt < /dev/null &
+python3 bot.py > output.txt 2> error.txt < /dev/null &
